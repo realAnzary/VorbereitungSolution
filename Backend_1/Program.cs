17ef02e2c4c9f41acdaaff9cc30f2cc8415c2566
AnzaryPC <unger.marius@web.de>
2022-03-13 14:31:31 +0100

    Frontend Zeugs verlegt

--- Backend_1/Program.cs
+++ Backend_1/Program.cs
@@ -2,39 +2,39 @@
 
 namespace Backend_1
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			for (int i = 0; i < 100; i++)
-			{
-				Console.WriteLine(evaluateTernary(i +1));
-			}
-		}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(evaluateTernary(i + 1));
+            }
+        }
 
-		public static string evaluate(int num)
-		{
-			if (num % 5 == 0 && num % 3 == 0)
-			{
-				return "FizzBuzz";
-			}
-			else if (num % 5 == 0)
-			{
-				return "Buzz";
-			}
-			else if (num % 3 == 0)
-			{
-				return "Fizz";
-			}
-			else
-			{
-				return $"{num}";
-			}
-		}
-		public static string evaluateTernary(int num)
-		{
-			string result = "";
-			return result = (num % 5 == 0 & num % 3 == 0) ? "FizzBuzz" : (num % 5 == 0) ? "Buzz" : (num % 3 == 0) ? "Fizz" : $"{num}";
-		}
-	}
+        public static string evaluate(int num)
+        {
+            if (num % 5 == 0 && num % 3 == 0)
+            {
+                return "FizzBuzz";
+            }
+            else if (num % 5 == 0)
+            {
+                return "Buzz";
+            }
+            else if (num % 3 == 0)
+            {
+                return "Fizz";
+            }
+            else
+            {
+                return $"{num}";
+            }
+        }
+        public static string evaluateTernary(int num)
+        {
+            string result = "";
+            return result = (num % 5 == 0 & num % 3 == 0) ? "FizzBuzz" : (num % 5 == 0) ? "Buzz" : (num % 3 == 0) ? "Fizz" : $"{num}";
+        }
+    }
 }
