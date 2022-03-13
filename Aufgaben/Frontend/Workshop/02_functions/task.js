4598282959924197ec7869f21f43421b912b56a0
AnzaryPC <unger.marius@web.de>
2022-03-13 11:45:38 +0100

    Added Tasks

---
+++ Aufgaben/Frontend/Workshop/02_functions/task.js
@@ -0,0 +1,7 @@
+﻿function showError(error) {
+    var errorMessage = "Achtung, folgender Fehler ist aufgetreten: ";
+    errorMessage += error;
+    var errorWarning = document.getElementById("ErrorWarnings");
+    errorWarning.innerText = errorMessage;
+    errorWarning.style.display = "block";
+}
---
+++ Aufgaben/Frontend/Workshop/04_moveContainer/task.js
@@ -0,0 +1,5 @@
+﻿// 2. Ändere die Hintergrund- und Randfarbe vom Container #MoveContainer
+
+// 3. Hänge einen click-Eventlistener an den Button #ToggleMoveContainerBtn, der den Container #MoveContainer ein- bzw. ausblendet
+
+// 4. Erstelle einen zweiten Button #ToggleContainerBtn, der per Klick den Container #MoveContainer nach rechts bzw. links verschiebt.
