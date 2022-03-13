a9e19700a1f6e5b429a04c9287a2e1cea515d77b
AnzaryPC <unger.marius@web.de>
2022-03-13 10:20:08 +0100

    Add project files.

---
+++ Backend_1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Backend_1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(evaluateTernary(i +1));
+            }
+		}
+
+		public static string evaluate(int number)
+        {
+            if (number % 5 == 0 && number % 3 == 0)
+            {
+				return "FizzBuzz";
+            }
+            else if (number % 5 == 0)
+            {
+                return "Buzz";
+            }
+            else if (number % 3 == 0)
+            {
+                return "Fizz";
+            }
+            else
+            {
+                return $"{number}";
+            }
+        }
+        public static string evaluateTernary(int num)
+        {
+            string result = "";
+
+            result = (num % 5 == 0 & num % 3 == 0) ? "FizzBuzz" : (num % 5 == 0) ? "Buzz" : (num % 3 == 0) ? "Fizz" : $"{num}";
+
+            return result;
+        }
+	}
+}
