3082367bbd69f2419abf1ef16de239b0e65ab909
AnzaryPC <unger.marius@web.de>
2022-03-13 13:04:04 +0100

    Small Fix

--- Backend_1/Program.cs
+++ Backend_1/Program.cs
@@ -6,38 +6,36 @@ namespace Backend_1
 	{
 		static void Main(string[] args)
 		{
-            for (int i = 0; i < 100; i++)
-            {
-                Console.WriteLine(evaluateTernary(i +1));
-            }
+			for (int i = 0; i < 100; i++)
+			{
+				Console.WriteLine(evaluateTernary(i +1));
+			}
 		}
 
 		public static string evaluate(int num)
-        {
-            if (num % 5 == 0 && num % 3 == 0)
-            {
+		{
+			if (num % 5 == 0 && num % 3 == 0)
+			{
 				return "FizzBuzz";
-            }
-            else if (num % 5 == 0)
-            {
-                return "Buzz";
-            }
-            else if (num % 3 == 0)
-            {
-                return "Fizz";
-            }
-            else
-            {
-                return $"{num}";
-            }
-        }
-        public static string evaluateTernary(int num)
-        {
-            string result = "";
+			}
+			else if (num % 5 == 0)
+			{
+				return "Buzz";
+			}
+			else if (num % 3 == 0)
+			{
+				return "Fizz";
+			}
+			else
+			{
+				return $"{num}";
+			}
+		}
+		public static string evaluateTernary(int num)
+		{
+			string result = "";
 
-            result = (num % 5 == 0 & num % 3 == 0) ? "FizzBuzz" : (num % 5 == 0) ? "Buzz" : (num % 3 == 0) ? "Fizz" : $"{num}";
-
-            return result;
-        }
+			 return result = (num % 5 == 0 & num % 3 == 0) ? "FizzBuzz" : (num % 5 == 0) ? "Buzz" : (num % 3 == 0) ? "Fizz" : $"{num}";
+		}
 	}
 }
