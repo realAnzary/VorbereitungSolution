a6d65115e111b09f47189aef6ffb9d6388d45de8
AnzaryPC <unger.marius@web.de>
2022-03-14 11:53:31 +0100

    Task2

--- Frontend/02_functions/task.js
+++ Frontend/02_functions/task.js
@@ -5,3 +5,34 @@
     errorWarning.innerText = errorMessage;
     errorWarning.style.display = "block";
 }
+
+function clearError() {
+    errorfield = document.getElementById("ErrorWarnings")
+    errorfield.innerText = "";
+}
+
+var helloWorldBtn = document.getElementById("btn-submit");
+helloWorldBtn.addEventListener("click", function() {
+    input = document.getElementById("TextInput");
+    alert(`Willkommen auf der Seite, ${input.value}`);
+});
+
+var checkBtn = document.getElementById("btn-check");
+checkBtn.addEventListener("click", function() {
+    input = document.getElementById("TextInput");
+    if (!isNaN(input.value)) {
+        input.value = input.value * 2;
+    } else {
+        showError("Eingabe ist keine Zahl oder beinhaltet Zeichen");
+    }
+});
+
+var clearBtn = document.getElementById("btn-clear");
+clearBtn.addEventListener("click", function() {
+    clearError()
+});
+
+var calcBtn = document.getElementById("btn-calculate");
+calcBtn.addEventListener("click", function() {
+    //ToDO
+})
