d3a5f66d986c2e533bd225bd0fe9ea707b31fdee
AnzaryLaptop <unger.marius@web.de>
2022-03-18 20:19:22 +0100

    idk

--- Frontend/03_tables/task.js
+++ Frontend/03_tables/task.js
@@ -18,18 +18,6 @@ function read() {
 
     header = table.createTHead();
     header.innerHTML = "<tr><td>Substring</td><td>Häufigkeit</td></tr>";
-    /*
-
-    tabel.appendChild(tblBody)
-
-    celldata = document.createTextNode("Hello")
-    cell = document.createElement("td")
-    cell.appendChild(celldata)
-
-    row = document.createElement("tr")
-    row.appendChild(cell)
-    tblBody.appendChild(row)
-    */
 
     const uniques = new Set(seperatedStrings);
     //console.log(uniques);
@@ -84,7 +72,7 @@ function sort() {
             x = rows[i].getElementsByTagName("td")[1];
             y = rows[i + 1].getElementsByTagName("td")[1];
 
-            if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+            if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
                 makeSwitch = true;
                 break;
             }
--- Frontend/04_moveContainer/task.js
+++ Frontend/04_moveContainer/task.js
@@ -1,5 +1,58 @@
 ﻿// 2. Ändere die Hintergrund- und Randfarbe vom Container #MoveContainer
-
+var moveContainer = document.getElementById("move-container");
+moveContainer.style.backgroundColor = "green";
+moveContainer.style.borderColor = "yellow";
 // 3. Hänge einen click-Eventlistener an den Button #ToggleMoveContainerBtn, der den Container #MoveContainer ein- bzw. ausblendet
+var isVisible = true;
+var btnVisibilty = document.getElementById("ToggleMoveContainerBtn");
+btnVisibilty.addEventListener("click", () => {
+    if (isVisible) {
+        console.log("Hide");
+        moveContainer.style.display = "none";
+        btnVisibilty.value = "Zeigen";
+        isVisible = false;
+    } else {
+        console.log("Show");
+        moveContainer.style.display = "block";
+        btnVisibilty.value = "Verstecken";
+        isVisible = true;
+    }
+})
+
+
+var moveToRight = true;
+var moveContainerBtn = document.getElementById("move-container-btn");
+moveContainerBtn.addEventListener("click", () => {
+    leftPos = moveContainer.style.left.replace("px", "");
+    stepsInput = document.getElementById("move-container-steps")
+    steps = parseInt(stepsInput.value)
+
+    if (moveToRight && leftPos > screen.width) {
+        moveToRight = false;
+    }
+    if (!moveToRight && leftPos < 0) {
+        moveToRight = true;
+    }
+
+    if (leftPos != "") {
+        leftPos = parseInt(leftPos);
+    }
+
+    if (moveToRight) {
+        moveContainer.style.left = `${leftPos + steps}px`;
+    } else {
+        moveContainer.style.left = `${leftPos - steps}px`;
+    }
+})
+
+var stepsBox = document.getElementById("move-container-steps");
+stepsBox.addEventListener("focusout", () => {
+    stepsBox.value = 100;
+})
+stepsBox.addEventListener("change", () => {
+    if (stepsBox.value == "") {
+        stepsBox.value = 100;
+    }
+})
 
 // 4. Erstelle einen zweiten Button #ToggleContainerBtn, der per Klick den Container #MoveContainer nach rechts bzw. links verschiebt.
