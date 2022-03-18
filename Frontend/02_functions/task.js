9ba1dedae40b3aa57fb63962ed293a746c6f2e77
AnzaryLaptop <unger.marius@web.de>
2022-03-18 12:42:50 +0100

    Finished Task 2 and 3

--- Frontend/02_functions/task.js
+++ Frontend/02_functions/task.js
@@ -6,6 +6,12 @@
     errorWarning.style.display = "block";
 }
 
+function isNumeric(str) {
+    if (typeof str != "string") return false
+    return !isNaN(str) &&
+        !isNaN(parseFloat(str))
+}
+
 function clearError() {
     errorfield = document.getElementById("ErrorWarnings")
     errorfield.innerText = "";
@@ -13,13 +19,13 @@ function clearError() {
 
 var helloWorldBtn = document.getElementById("btn-submit");
 helloWorldBtn.addEventListener("click", function() {
-    input = document.getElementById("TextInput");
+    input = document.getElementById("text-input");
     alert(`Willkommen auf der Seite, ${input.value}`);
 });
 
 var checkBtn = document.getElementById("btn-check");
 checkBtn.addEventListener("click", function() {
-    input = document.getElementById("TextInput");
+    input = document.getElementById("text-input");
     if (!isNaN(input.value)) {
         input.value = input.value * 2;
     } else {
@@ -34,5 +40,41 @@ clearBtn.addEventListener("click", function() {
 
 var calcBtn = document.getElementById("btn-calculate");
 calcBtn.addEventListener("click", function() {
-    //ToDO
+    number1 = document.getElementById("number-input1");
+    number2 = document.getElementById("number-input2");
+    out = document.getElementById("number-output");
+    console.log(`Num1:${number1.value} | Num2:${number2.value}`);
+
+    if (isNumeric(number1.value)) {
+        console.log("Num1 is Number");
+    } else { showError("Input 1 not a Number"); };
+
+    if (isNumeric(number2.value)) {
+        console.log("Num2 is Number");
+    } else { showError("Input 2 not a Number"); };
+
+    if (isNumeric(number1.value) && isNumeric(number2.value)) {
+        operations = document.getElementById("select-math");
+        switch (operations.value) {
+            case "+":
+                console.log("Operation: +");
+                out.value = +number1.value + +number2.value;
+                break;
+
+            case "-":
+                console.log("Operation: -");
+                out.value = +number1.value - +number2.value;
+                break;
+
+            case "*":
+                console.log("Operation: *");
+                out.value = +number1.value * +number2.value;
+                break;
+
+            case "/":
+                console.log("Operation: /");
+                out.value = +number1.value / +number2.value;
+                break;
+        }
+    }
 })
--- Frontend/03_tables/task.js
+++ Frontend/03_tables/task.js
@@ -1,1 +1,111 @@
-﻿
+﻿function invert(str) {
+    //console.log(`Input: ${str}`);
+    return str.split("").reverse().join("");
+}
+
+function read() {
+    input = document.getElementById("input-area");
+    inputString = input.value;
+    input.value = "";
+
+    seperatedStrings = inputString.split(",");
+    //console.log(seperatedStrings)
+
+    table = document.getElementById("table-output");
+    tblBody = document.createElement("tbody");
+    table.appendChild(tblBody);
+    table.innerHTML = "";
+
+    header = table.createTHead();
+    header.innerHTML = "<tr><td>Substring</td><td>Häufigkeit</td></tr>";
+    /*
+
+    tabel.appendChild(tblBody)
+
+    celldata = document.createTextNode("Hello")
+    cell = document.createElement("td")
+    cell.appendChild(celldata)
+
+    row = document.createElement("tr")
+    row.appendChild(cell)
+    tblBody.appendChild(row)
+    */
+
+    const uniques = new Set(seperatedStrings);
+    //console.log(uniques);
+    tbody = document.createElement("tbody");
+
+    uniques.forEach(element => {
+        currentSubString = element;
+
+        counter = 0;
+        seperatedStrings.forEach(object => {
+            if (object == element) {
+                counter++;
+            };
+        });
+        //console.log(`Element: ${element} occured ${counter} times`);
+
+
+        row = document.createElement("tr");
+        tableObject = document.createElement("td");
+        tableCounter = document.createElement("td");
+        dataObject = document.createTextNode(element);
+        dataCounter = document.createTextNode(counter);
+
+        tableObject.appendChild(dataObject);
+        tableCounter.appendChild(dataCounter);
+
+        row.appendChild(tableObject);
+        row.appendChild(tableCounter);
+
+
+        tbody.appendChild(row);
+
+
+    });
+
+    table.appendChild(tbody);
+    shouldSort = document.getElementById("checkbox-sort");
+    if (shouldSort.checked) {
+        sort()
+    }
+}
+
+function sort() {
+    const table = document.getElementById("table-output");
+    switching = true;
+
+    while (switching) {
+        switching = false;
+        rows = table.rows;
+        for (i = 0; i < (rows.length - 1); i++) {
+            makeSwitch = false;
+            x = rows[i].getElementsByTagName("td")[1];
+            y = rows[i + 1].getElementsByTagName("td")[1];
+
+            if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                makeSwitch = true;
+                break;
+            }
+        }
+        if (makeSwitch) {
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+        }
+    }
+
+}
+
+var btnInvert = document.getElementById("btn-invert");
+btnInvert.addEventListener("click", () => {
+    input = document.getElementById("input-area");
+    //console.log(invert(input.value));
+    input.value = invert(input.value);
+})
+
+var btnEvaluate = document.getElementById("btn-evaluate");
+btnEvaluate.addEventListener("click", () => {
+    //console.log("Do smth");
+    read()
+})
