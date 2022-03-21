670578dd14fbb329d5a4583476572cef7ca87747
AnzaryPC <unger.marius@web.de>
2022-03-21 11:02:29 +0100

    Backend2

--- Backend_1/Program.cs
+++ Backend_1/Program.cs
@@ -1,40 +1,30 @@
-﻿using System;
-
-namespace Backend_1
+﻿for (int i = 0; i < 99; i++)
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            for (int i = 0; i < 100; i++)
-            {
-                Console.WriteLine(evaluateTernary(i + 1));
-            }
-        }
+    Console.WriteLine(evaluateTernary(i + 1));
+}
 
-        public static string evaluate(int num)
-        {
-            if (num % 5 == 0 && num % 3 == 0)
-            {
-                return "FizzBuzz";
-            }
-            else if (num % 5 == 0)
-            {
-                return "Buzz";
-            }
-            else if (num % 3 == 0)
-            {
-                return "Fizz";
-            }
-            else
-            {
-                return $"{num}";
-            }
-        }
-        public static string evaluateTernary(int num)
-        {
-            string result = "";
-            return result = (num % 5 == 0 & num % 3 == 0) ? "FizzBuzz" : (num % 5 == 0) ? "Buzz" : (num % 3 == 0) ? "Fizz" : $"{num}";
-        }
+static string evaluate(int number)
+{
+    if (number % 5 == 0 && number % 3 == 0)
+    {
+        return "FizzBuzz";
     }
+    else if (number % 5 == 0)
+    {
+        return "Buzz";
+    }
+    else if (number % 3 == 0)
+    {
+        return "Fizz";
+    }
+    else
+    {
+        return $"{number}";
+    }
+}
+
+static string evaluateTernary(int number)
+{
+    string result = "";
+    return result = (number % 5 == 0 && number % 3 == 0) ? "FizzBuzz" : (number % 5 == 0) ? "Buzz" : (number % 3 == 0) ? "Fizz" : $"{number}";
 }
---
+++ Backend_2/Program.cs
@@ -0,0 +1,121 @@
+﻿string[] shape_circle = new[] { "kreis", "circle" };
+string[] shape_rectangle = new[] { "rechteck", "rectangle" };
+string[] shape_triangle = new[] { "dreieck", "triangle" };
+string[] string_quit = new[] { "quit", "close", "exit", "verlassen", "q" };
+
+while (true)
+{
+    Console.WriteLine("\"q\", \"quit\", \"close\", \"exit\", \"verlassen\" zum beenden!");
+    Console.WriteLine("Zulässige Eingaben:\n \"Dreieck\", \"Triangle\", \"Rechteck\", \"Rectangle\", \"Kreis\", \"Circle\"");
+    Console.WriteLine();
+    Console.Write("Eingabe der Form: ");
+    string? eingabe = Console.ReadLine().ToLower();
+
+    if (shape_circle.Contains(eingabe))
+    {
+        Console.WriteLine("Circle ausgewählt");
+        calc_circle();
+    }
+    else if (shape_rectangle.Contains(eingabe))
+    {
+        Console.WriteLine("Rectangle ausgewählt");
+        calc_rectangle();
+    }
+    else if (shape_triangle.Contains(eingabe))
+    {
+        Console.WriteLine("Triangle ausgewählt");
+        calc_Triangle();
+    }
+    else if (string_quit.Contains(eingabe))
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Keine richtige Eingabe");
+    }
+    Console.WriteLine();
+}
+
+void calc_circle()
+{
+    Console.WriteLine("Berechne Circle...");
+    double radius = 0.0d;
+    bool wrong_input = false;
+
+    do
+    {
+        wrong_input = false;
+        try
+        {
+            Console.Write("Radius eingeben (in cm): ");
+            radius = Convert.ToDouble(Console.ReadLine());
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.GetType()} | {ex.Message}");
+            wrong_input = true;
+        }
+    } while (wrong_input);
+
+    Console.WriteLine($"Gewählter Radius: {radius}");
+    Console.WriteLine($"Flächeninhalt: {Math.Round(Math.PI * Math.Pow(radius, 2), 2)}cm^2 Umfang: {Math.Round((2 * Math.PI * radius), 2)}cm");
+}
+
+void calc_rectangle()
+{
+    Console.WriteLine("Berechne Rectangle...");
+    double site_a = 0.0d;
+    double site_b = 0.0d;
+    bool wrong_input = false;
+
+    do
+    {
+        wrong_input = false;
+        try
+        {
+            Console.Write("Seitenlänge A eingeben (in cm): ");
+            site_a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Seitenlänge B eingeben (in cm): ");
+            site_b = Convert.ToDouble(Console.ReadLine());
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.GetType()} | {ex.Message}");
+            wrong_input = true;
+        }
+    } while (wrong_input);
+
+    Console.WriteLine($"Gewählte Seitenlänge A: {site_a}cm B: {site_b}cm");
+    Console.WriteLine($"Flächeninhalt: {Math.Round(site_a * site_b, 2)}cm^2 Umfang: {Math.Round(2 * (site_a + site_b), 2)}cm");
+}
+
+void calc_Triangle()
+{
+    Console.WriteLine("Berechne Triangle...");
+    double site_a = 0.0d;
+    double site_b = 0.0d;
+    double site_c = 0.0d;
+    bool wrong_input = false;
+
+    do
+    {
+        wrong_input = false;
+        try
+        {
+            Console.Write("Seitenlänge A(Kathete) eingeben (in cm): ");
+            site_a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Seitenlänge B(Kathete) eingeben (in cm): ");
+            site_b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Seitenlänge C(Hypothenuse) eingeben (in cm): ");
+            site_c = Convert.ToDouble(Console.ReadLine());
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.GetType()} | {ex.Message}");
+            wrong_input = true;
+        }
+    } while (wrong_input);
+    Console.WriteLine($"Gewählte Seitenlänge A: {site_a}cm B: {site_b}cm C: {site_c}cm");
+    Console.WriteLine($"Flächeninhalt: {Math.Round((site_a * site_b)) * .5, 2}cm^2 Umfang: {Math.Round(site_a + site_b + site_c, 2)}cm");
+}
